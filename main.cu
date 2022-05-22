17f7f9f8b90a3a40a0685aef51ea60c089998079
Emil Nordén <emilnorden@yahoo.se>
2022-05-22 19:50:05 +0200

    small fix

--- main.cu
+++ main.cu
@@ -10,6 +10,8 @@ __global__ void add_kernel(float *a, float *b, float *result, size_t N) {
 
 template <size_t N>
 std::array<float, N> add_on_gpu(const std::array<float, N> &a, const std::array<float, N> &b) {
+    std::array<float, N> results{};
+    // Get some data onto the GPU
     float *device_array_a;
     cudaMalloc(&device_array_a, sizeof(float) * N);
     cudaMemcpy(device_array_a, a.data(), sizeof(float) * N, cudaMemcpyHostToDevice);
@@ -21,9 +23,10 @@ std::array<float, N> add_on_gpu(const std::array<float, N> &a, const std::array<
     float *device_results;
     cudaMalloc(&device_results, sizeof(float) * N);
 
+    // Run our fancy CUDA logic
     add_kernel<<<1, N>>>(device_array_a, device_array_b, device_results, N);
 
-    std::array<float, N> results{};
+    // Get our data back from the GPU
     cudaMemcpy(results.data(), device_results, sizeof(float) * N, cudaMemcpyDeviceToHost);
 
     return results;
@@ -33,7 +36,7 @@ int main() {
 
     auto result = add_on_gpu<3>(
             {1,2,3},
-            {20,30,40});
+            {10,20,30});
 
     for(auto x : result) {
         std::cout << x << "\n";
